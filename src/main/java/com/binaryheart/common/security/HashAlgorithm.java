921b69309acb8a048633fd9cd8e224840898decb
Farbod Safaei <farbod.safaei@gmail.com>
2016-03-21 02:57:59 -0700

    javadoc updates

--- src/main/java/com/binaryheart/common/security/HashAlgorithm.java
+++ src/main/java/com/binaryheart/common/security/HashAlgorithm.java
@@ -28,32 +28,32 @@ public enum HashAlgorithm {
 
  
     /**
-     * String representation of  algorithm
+     * String representation of PBKDF2WithHmacMD5 algorithm
      */
     PBKDF2WITHHMACMD5("PBKDF2WithHmacMD5"),
     
     /**
-     * String representation of  algorithm
+     * String representation of PBKDF2WithHmacSHA1 algorithm
      */
     PBKDF2WITHHMACSHA1("PBKDF2WithHmacSHA1"),
     
     /**
-     * String representation of  algorithm
+     * String representation of PBKDF2WithHmacSHA224 algorithm
      */
     PBKDF2WITHHMACSHA224("PBKDF2WithHmacSHA224"),
     
     /**
-     * String representation of  algorithm
+     * String representation of PBKDF2WithHmacSHA256 algorithm
      */
     PBKDF2WITHHMACSHA256("PBKDF2WithHmacSHA256"),
     
     /**
-     * String representation of  algorithm
+     * String representation of PBKDF2WithHmacSHA384 algorithm
      */
     PBKDF2WITHHMACSHA384("PBKDF2WithHmacSHA384"),
     
     /**
-     * String representation of  algorithm
+     * String representation of PBKDF2WithHmacSHA512 algorithm
      */
     PBKDF2WITHHMACSHA512("PBKDF2WithHmacSHA512");
     
